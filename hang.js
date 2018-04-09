cb7d17f181f504c3cf89c03f6cfae1324b049594
Ben <b.wayne@me.com>
2018-04-09 16:42:34 -0600

    finished gallows

--- hang.js
+++ hang.js
@@ -78,24 +78,64 @@ function arm1() {
   console.log("\x1b[37m%s\x1b[0m","    ______________|");
 }
 
-function template() {
+function arm2() {
   console.log("\x1b[37m%s\x1b[0m","           ------- ");
   console.log("\x1b[37m%s\x1b[0m","          |       |");
-  console.log("\x1b[37m%s\x1b[0m","         ---      |");
-  console.log("\x1b[37m%s\x1b[0m","        |   |     |");
-  console.log("\x1b[37m%s\x1b[0m","       \\ --- /    |");
-  console.log("\x1b[37m%s\x1b[0m","        \\ | /     |");
-  console.log("\x1b[37m%s\x1b[0m","         \\|/      |");
-  console.log("\x1b[37m%s\x1b[0m","          |       |");
-  console.log("\x1b[37m%s\x1b[0m","          |       |");
-  console.log("\x1b[37m%s\x1b[0m","          |       |");
-  console.log("\x1b[37m%s\x1b[0m","          |       |");
-  console.log("\x1b[37m%s\x1b[0m","         /\\       |");
-  console.log("\x1b[37m%s\x1b[0m","        /  \\      |");
-  console.log("\x1b[37m%s\x1b[0m","       /    \\     |");
-  console.log("\x1b[37m%s\x1b[0m","      /      \\    |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","         --- ","     |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","        |   |","     |");
+  console.log("\x1b[31m%s\x1b[5m%s\x1b[0m\x1b[37m%s\x1b[0m","       \\ ---"," /","    |");
+  console.log("\x1b[31m%s\x1b[5m%s\x1b[0m\x1b[37m%s\x1b[0m","        \\ |"," /","     |");
+  console.log("\x1b[31m%s\x1b[5m%s\x1b[0m\x1b[37m%s\x1b[0m","         \\|","/","      |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","          |","       |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","          |","       |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","          |","       |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","          |","       |");
+  console.log("\x1b[37m%s\x1b[0m","                  |");
+  console.log("\x1b[37m%s\x1b[0m","                  |");
+  console.log("\x1b[37m%s\x1b[0m","                  |");
+  console.log("\x1b[37m%s\x1b[0m","                  |");
   console.log("\x1b[37m%s\x1b[0m","                  |");
   console.log("\x1b[37m%s\x1b[0m","    ______________|");
 }
 
-arm1();
+function leg1() {
+  console.log("\x1b[37m%s\x1b[0m","           ------- ");
+  console.log("\x1b[37m%s\x1b[0m","          |       |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","         --- ","     |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","        |   |","     |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","       \\ --- /","    |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","        \\ | /","     |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","         \\|/","      |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","          |","       |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","          |","       |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","          |","       |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","          |","       |");
+  console.log("\x1b[5m\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","         /","        |");
+  console.log("\x1b[5m\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","        /","         |");
+  console.log("\x1b[5m\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","       /","          |");
+  console.log("\x1b[5m\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","      /","           |");
+  console.log("\x1b[37m%s\x1b[0m","                  |");
+  console.log("\x1b[37m%s\x1b[0m","    ______________|");
+}
+
+function leg2() {
+  console.log("\x1b[37m%s\x1b[0m","           ------- ");
+  console.log("\x1b[37m%s\x1b[0m","          |       |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","         --- ","     |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","        |   |","     |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","       \\ --- /","    |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","        \\ | /","     |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","         \\|/","      |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","          |","       |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","          |","       |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","          |","       |");
+  console.log("\x1b[31m%s\x1b[0m\x1b[37m%s\x1b[0m","          |","       |");
+  console.log("\x1b[31m%s\x1b[5m%s\x1b[0m\x1b[37m%s\x1b[0m","         /","\\","       |");
+  console.log("\x1b[31m%s\x1b[5m%s\x1b[0m\x1b[37m%s\x1b[0m","        /","  \\","      |");
+  console.log("\x1b[31m%s\x1b[5m%s\x1b[0m\x1b[37m%s\x1b[0m","       /","    \\","     |");
+  console.log("\x1b[31m%s\x1b[5m%s\x1b[0m\x1b[37m%s\x1b[0m","      /","      \\","    |");
+  console.log("\x1b[37m%s\x1b[0m","                  |");
+  console.log("\x1b[37m%s\x1b[0m","    ______________|");
+}
+
+leg2();
